2e4a055c778b0c36156ba99b7f44839b8868e3b0
Atlegang Sethono <atlegangcode@gmail.com>
2024-05-12 16:39:36 +0200

    Add Client class to manage client-server communication
    This commit introduces the Client class responsible for managing the client-side communication with the server.
    The class provides methods to send messages to the server, listen for messages from the server, and handle user interaction.
    It includes functionality to establish a connection to the server, send messages along with the client's username, display messages received from the server.
    Additionally, the class ensures closure of sockets and streams to maintain a stable connection.
    
    Changes:
    - Added Client class to manage client-server communication.
    - Implemented methods for sending and listening to messages.
    - Included functionality for establishing a connection to the server and handling user input
    
    Fixes: None

---
+++ clientServer/src/main/java/org/example/client/Client.java
@@ -0,0 +1,97 @@
+package org.example.client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void sendMessage() {
+        try {
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isClosed()) {
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void listenForMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String messageFromServerWorld;
+
+                while (socket.isConnected()) {
+                    try {
+                        messageFromServerWorld = bufferedReader.readLine();
+                        System.out.println(messageFromServerWorld);
+                    } catch (IOException e) {
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                    }
+                }
+            }
+        }).start();;
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+
+            /** NOTE: 
+             * With streams, you only need to close the wrapper as the underlying streams are closed when you close the wrapper 
+             * So, closing bufferedReader, bufferedWriter does the job*/ 
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What would you like to name your robot?");
+        String robotName = scanner.nextLine();
+        Socket socket = new Socket("localhost", 5000);
+        Client client = new Client(socket, robotName);
+        client.listenForMessage();
+        client.sendMessage();
+    }
+
+}
